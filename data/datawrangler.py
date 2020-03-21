dfadc6663836bd0fbefe2fd13e08ae33bfe8fac0
davefoote <dfoote@uchicago.edu>
2020-03-20 23:44:29 -0500

    final push 1.0

--- data/datawrangler.py
+++ data/datawrangler.py
@@ -1,27 +1,36 @@
 import geopandas as gpd
+from shapely.geometry import Polygon, mapping
 
 '''
 First, convert baselayer shapefile to geojson:
 '''
 def main():
-    la_countylines = gpd.read_file('drp/DRP_COUNTY_BOUNDARY.shp')
-    la_countylines = la_countylines.to_crs(epsg=5070)
-    la_countylines.geometry = la_countylines.geometry.convex_hull
-
+    socal = gpd.read_file('https://s3-us-west-2.amazonaws.com/boundaries.latimes.com/archive/' +
+                          '1.0/boundary-set/county-subdivisions-2012.geojson')
+    socal['COUNTY'] = [x['COUNTYFP'] for x in socal.metadata]
+    socal = socal.dissolve(by="COUNTY")
     df = gpd.read_file('crithabs/crithabs.shp')
     df.crs = {'init':'epsg:5070'}
-    df.geometry = df.geometry.simplify(.01)
+    df = df.to_crs(epsg=4326)
+    df.geometry = df.geometry.simplify(.1)
 
-    la_habitats = gpd.overlay(la_countylines, df, how='intersection')
-    la_birds = la_habitats[la_habitats.species == 'Birds']
-    la_reptiles = la_habitats[la_habitats.species == 'Reptiles']
-    la_insects = la_habitats[la_habitats.species == 'Insects']
-    la_amphibians = la_habitats[la_habitats.species == 'Amphibians']
+    socal_habitats = gpd.overlay(socal, df, how='intersection')
+    socal.to_file('drp/socal.geojson', driver='GeoJSON')
 
-    la_birds.to_file('la_critters/la_birds.shp')
-    la_reptiles.to_file('la_critters/la_reptiles.shp')
-    la_insects.to_file('la_critters/la_insects.shp')
-    la_amphibians.to_file('la_critters/la_amphibians.shp')
+    la_birds = socal_habitats[la_habitats.species == 'Birds']
+    la_fplants = socal_habitats[la_habitats.species == 'Flowering Plants']
+    la_fishes = socal_habitats[la_habitats.species == 'Fishes']
+    la_crust = socal_habitats[la_habitats.species == 'Crustaceans']
+    la_amphibians = socal_habitats[la_habitats.species == 'Amphibians']
     
+    for x in [la_birds, la_fplants, la_fishes, la_crust, la_amphibians]:
+        x = x.dissolve(by="sciname")
+
+    la_birds.to_file('la_critters/la_birds.geojson', driver='GeoJSON')
+    la_fplants.to_file('la_critters/la_fplants.geojson', driver='GeoJSON')
+    la_fishes.to_file('la_critters/la_fishes.geojson', driver='GeoJSON')
+    la_crust.to_file('la_critters/la_crust.geojson', driver='GeoJSON')
+    la_amphibians.to_file('la_critters/la_amphibians.geojson', driver='GeoJSON')
+
 if __name__ == "__main__":
     main()
--- src/app.js
+++ src/app.js
@@ -1,51 +1,47 @@
 import * as d3 from 'd3';
 import './stylesheets/main.css';
-var topojsonSimplify = require("topojson-simplify");
+var topojsonSimplify = require("topojson-simplify")
+;
 
 // if the data you are going to import is small, then you can import it using es6 import
 // import MY_DATA from './app/data/example.json'
 // (I tend to think it's best to use screaming snake case for imported json)
-var loader = document.getElementsByClassName('loader');
-
-window.addEventListener ("load", function() {
-    loader[0].style.display = 'none';
-});
 const domReady = require('domready');
 
 const slideSpeciesTypeMap = {
     'Birds': birdsPlot,
-    'Insects': insectsPlot,
+    'Fishes': fishesPlot,
     'Amphibians': amphiPlot,
-    'Reptiles': reptilesPlot
+    'Flowering Plants': fplantsPlot,
+    'Crustaceans': crustPlot
 };
 
-var palette = ["#A2A8A8", "#D37E34", "#18872C", "#8C271E", "#3F89E2", "#EC7A8E"]
-
 domReady(() => {
-    Promise.all([d3.json('./data/drp/DRP_COUNTY_BOUNDARY.geojson'),
+    Promise.all([d3.json('./data/drp/socal.geojson'),
                 d3.json('./data/la_critters/la_amphibians.geojson'),
                 d3.json('./data/la_critters/la_birds.geojson'),
-                d3.json('./data/la_critters/la_insects.geojson'),
-                d3.json('./data/la_critters/la_reptiles.geojson')])
+                d3.json('./data/la_critters/la_fplants.geojson'),
+                d3.json('./data/la_critters/la_fishes.geojson'),
+                d3.json('./data/la_critters/la_crust.geojson')])
       .then(
         d => {
-            const [back, amphi, birds, insects, reptiles] = d;
+            const [back, amphi, birds, fplants, fishes, crust] = d;
             console.log(d)
-            application(back, amphi, birds, insects, reptiles);
+            application(back, amphi, birds, fplants, fishes, crust);
         },
     );
     
 });
 
 
-function application(data, amphi, birds, insects, reptiles) {
+function application(data, amphi, birds, fplants, fishes, crust) {
   const state = {slideIdx: 0}
   console.log('birbs!', birds)
   laChart(data);
     
   d3.select('.buttons-container')
       .selectAll('button')
-      .data(['Birds', 'Insects', 'Amphibians', 'Reptiles'])
+      .data(['Birds', 'Flowering Plants', 'Amphibians', 'Fishes', 'Crustaceans'])
       .enter()
       .append('button')
       .text(d => d)
@@ -54,17 +50,15 @@ function application(data, amphi, birds, insects, reptiles) {
         console.log("last: ", last_state)
         state.slideIdx = d;
           if (state.slideIdx === 'Birds') {
-            render(state, birds, last_state)
-            console.log(d);};
-          if (state.slideIdx === 'Insects') {
-            render(state, insects, last_state)
-            console.log(d);};
-          if (state.slideIdx === 'Reptiles') {
-            render(state, reptiles, last_state)
-            console.log(d);};
+            render(state, birds, last_state);};
+          if (state.slideIdx === 'Flowering Plants') {
+            render(state, fplants, last_state);};
+          if (state.slideIdx === 'Fishes') {
+            render(state, fishes, last_state);};
+          if (state.slideIdx === 'Crustaceans') {
+            render(state, crust, last_state);};
           if (state.slideIdx === 'Amphibians') {
-            render(state, amphi, last_state)
-            console.log(d);};
+            render(state, amphi, last_state);};
           });
  
 }
@@ -78,29 +72,39 @@ function render(state, data, last) {
     d3.select('#mapzone')
       .append('h1')
       .text(state.slideIdx);
+            var chartConfig = {
+            target : 'chart',
+            data_url : 'external_data.json',
+            width: 900,
+            height: 450,
+            val: 90
+        };
     
     if (state.slideIdx === 'Birds') {
-        birdsPlot(data)
+          birdsPlot(data);
     };
     
-    if (state.slideIdx === 'Insects') {
-        insectsPlot(data)
+    if (state.slideIdx === 'Flowering Plants') {
+        fplantsPlot(data)
     };
     if (state.slideIdx === 'Amphibians') {
         amphiPlot(data)
     };
-    if (state.slideIdx === 'Reptiles') {
-        reptilesPlot(data)
+    if (state.slideIdx === 'Fishes') {
+        fishesPlot(data);
+    };
+    if (state.slideIdx === 'Crustaceans') {
+        crustPlot(data)
     };
 };
 
 function laChart(data) {
-    var width = 960;
-    var height = 800;
+    var width = 1000;
+    var height = 400;
     
     var projection = d3.geoAlbersUsa()
-                    .scale(26000)
-                    .translate([width * 8.75, -800]);
+                    .scale(6000)
+                    .translate([width * 2.17, 10]);
     var path = d3.geoPath()
             .projection(projection);
     var svg = d3.select("body")
@@ -114,20 +118,21 @@ function laChart(data) {
             .attr("d", path)
             .attr("width", width)
             .attr("height", height)
-            .style("stroke", "#fff")
+            .style("stroke", "#605A50")
             .style("stroke-width", "1")
-            .style("fill", "#A2A8A8");
+            .style("fill", "#fff");            
 };
 
 function birdsPlot(habs) {
+    console.log(document.readyState);
     var div = d3.select("body").append("div")	
             .attr("class", "tooltip")				
             .style("opacity", 0);
-    var width = 960;
-    var height = 500;
+    var width = 1000;
+    var height = 400;
     var projection = d3.geoAlbersUsa()
-                .scale(26000)
-                .translate([width * 8.75, -800]);
+                    .scale(6000)
+                    .translate([width * 2.17, 10]);
     var path = d3.geoPath()
             .projection(projection);
     var svg = d3.select("body")
@@ -145,12 +150,13 @@ function birdsPlot(habs) {
           .on("mouseover", function(d) {		
             div.transition()		
                 .duration(200)		
-                .style("opacity", .9);		
-            div.html(d.properties.Scientific + "    Commonly known as: " + 
-                     d.properties["Common Nam"])
+                .style("opacity", .75);		
+            div.html(d.properties.sciname + "    Status: " + 
+                     d.properties["status"])
                 .style("left", (d3.event.pageX) + "px")		
                 .style("top", (d3.event.pageY - 28) + "px")
-            console.log(d.properties.Scientific);	
+            console.log(d.properties.status)
+            console.log(d.properties.sciname);	
             })
           .on("mouseout", function(d) {		
             div.transition()		
@@ -158,23 +164,23 @@ function birdsPlot(habs) {
                 .style("opacity", 0);	
         })
           .on("click", function(d) {
-            window.open("https://en.wikipedia.org/wiki/" + d.properties.Scientific, "_blank");
+            window.open("https://en.wikipedia.org/wiki/" + d.properties.sciname, "_blank");
           })
-          .style("stroke", "#fff")
-          .style("stroke-width", "1")
-          .style("fill", "#3F89E2");
+          .style("stroke", "#392F16")
+          .style("stroke-width", "3")
+          .style("fill", "#392F16");
           console.log("feats: ", habs.features);
 };
 
-function insectsPlot(habs) {
+function fplantsPlot(habs) {
     var div = d3.select("body").append("div")	
             .attr("class", "tooltip")				
             .style("opacity", 0);
-    var width = 960;
-    var height = 500;
+    var width = 1000;
+    var height = 400;
     var projection = d3.geoAlbersUsa()
-                    .scale(26000)
-                    .translate([width * 8.75, -800]);
+                    .scale(6000)
+                    .translate([width * 2.17, 10]);
     var path = d3.geoPath()
             .projection(projection);
     var svg = d3.select("body")
@@ -186,31 +192,32 @@ function insectsPlot(habs) {
             .enter()
             .append("g")
             .attr("class", "collection");
-    g.selectAll("path")
-            .data(habs.features).enter()
+    svg.selectAll("path")
+          .data(habs.features).enter()
           .append("path")
           .attr("d", path)
           .on("mouseover", function(d) {		
             div.transition()		
                 .duration(200)		
-                .style("opacity", .9);		
-            div.html(d.properties.Scientific + "    Commonly known as: " + 
-                     d.properties["Common Nam"])
+                .style("opacity", .75);		
+            div.html(d.properties.sciname + "    Status: " + 
+                     d.properties["status"])
                 .style("left", (d3.event.pageX) + "px")		
                 .style("top", (d3.event.pageY - 28) + "px")
-            console.log(d.properties.Scientific);	
+            console.log(d.properties.status)
+            console.log(d.properties.sciname);	
             })
           .on("mouseout", function(d) {		
             div.transition()		
                 .duration(500)		
                 .style("opacity", 0);	
-            })
-           .on("click", function(d) {
-            window.open("https://en.wikipedia.org/wiki/" + d.properties.Scientific, "_blank");
-            })
-          .style("stroke", "#fff")
-          .style("stroke-width", "1")
-          .style("fill", "#3F89E2");
+        })
+          .on("click", function(d) {
+            window.open("https://en.wikipedia.org/wiki/" + d.properties.sciname, "_blank");
+          })
+          .style("stroke", "#253C78")
+          .style("stroke-width", "3")
+          .style("fill", "#253C78");
           console.log("feats: ", habs.features);
 };
 
@@ -218,11 +225,11 @@ function amphiPlot(habs) {
     var div = d3.select("body").append("div")	
             .attr("class", "tooltip")				
             .style("opacity", 0);
-    var width = 960;
-    var height = 500;
+    var width = 1000;
+    var height = 400;
     var projection = d3.geoAlbersUsa()
-                    .scale(26000)
-                    .translate([width * 8.75, -800]);
+                    .scale(6000)
+                    .translate([width * 2.17, 10]);
     var path = d3.geoPath()
             .projection(projection);
     var svg = d3.select("body")
@@ -234,43 +241,44 @@ function amphiPlot(habs) {
             .enter()
             .append("g")
             .attr("class", "collection");
-    g.selectAll("path")
-            .data(habs.features).enter()
+    svg.selectAll("path")
+          .data(habs.features).enter()
           .append("path")
           .attr("d", path)
           .on("mouseover", function(d) {		
             div.transition()		
                 .duration(200)		
-                .style("opacity", .9);		
-            div.html(d.properties.Scientific + "    Commonly known as: " + 
-                     d.properties["Common Nam"])
+                .style("opacity", .75);		
+            div.html(d.properties.sciname + "    Status: " + 
+                     d.properties["status"])
                 .style("left", (d3.event.pageX) + "px")		
                 .style("top", (d3.event.pageY - 28) + "px")
-            console.log(d.properties.Scientific);	
+            console.log(d.properties.status)
+            console.log(d.properties.sciname);	
             })
           .on("mouseout", function(d) {		
             div.transition()		
                 .duration(500)		
                 .style("opacity", 0);	
         })
-           .on("click", function(d) {
-            window.open("https://en.wikipedia.org/wiki/" + d.properties.Scientific, "_blank");
+          .on("click", function(d) {
+            window.open("https://en.wikipedia.org/wiki/" + d.properties.sciname, "_blank");
           })
-          .style("stroke", "#fff")
-          .style("stroke-width", "1")
-          .style("fill", "#3F89E2");
+          .style("stroke", "#D36582")
+          .style("stroke-width", "3")
+          .style("fill", "#D36582");
           console.log("feats: ", habs.features);
 };
 
-function reptilesPlot(habs) {
+function fishesPlot(habs) {
     var div = d3.select("body").append("div")	
             .attr("class", "tooltip")				
             .style("opacity", 0);
-    var width = 960;
-    var height = 500;
+    var width = 1000;
+    var height = 400;
     var projection = d3.geoAlbersUsa()
-                    .scale(26000)
-                    .translate([width * 8.75, -800]);
+                    .scale(6000)
+                    .translate([width * 2.17, 13]);
     var path = d3.geoPath()
             .projection(projection);
     var svg = d3.select("body")
@@ -282,31 +290,81 @@ function reptilesPlot(habs) {
             .enter()
             .append("g")
             .attr("class", "collection");
-    g.selectAll("path")
-            .data(habs.features).enter()
+     svg.selectAll("path")
+          .data(habs.features).enter()
           .append("path")
           .attr("d", path)
           .on("mouseover", function(d) {		
             div.transition()		
                 .duration(200)		
-                .style("opacity", .9);		
-            div.html(d.properties.Scientific + "    Commonly known as: " + 
-                     d.properties["Common Nam"])	
+                .style("opacity", .75);		
+            div.html(d.properties.sciname + "    Status: " + 
+                     d.properties["status"])
                 .style("left", (d3.event.pageX) + "px")		
                 .style("top", (d3.event.pageY - 28) + "px")
-            console.log(d.properties.Scientific);	
+            console.log(d.properties.status)
+            console.log(d.properties.sciname);	
             })
           .on("mouseout", function(d) {		
             div.transition()		
                 .duration(500)		
                 .style("opacity", 0);	
         })
-           .on("click", function(d) {
-            window.open("https://en.wikipedia.org/wiki/" + d.properties.Scientific, "_blank");
+          .on("click", function(d) {
+            window.open("https://en.wikipedia.org/wiki/" + d.properties.sciname, "_blank");
           })
-          .style("stroke", "#fff")
-          .style("stroke-width", "1")
-          .style("fill", "#3F89E2");
+          .style("stroke", "#E4572E")
+          .style("stroke-width", "3")
+          .style("fill", "#E4572E");
+          console.log("feats: ", habs.features);
+};
+
+function crustPlot(habs) {
+    var div = d3.select("body").append("div")	
+            .attr("class", "tooltip")				
+            .style("opacity", 0);
+    var width = 1000;
+    var height = 400;
+    var projection = d3.geoAlbersUsa()
+                    .scale(6000)
+                    .translate([width * 2.17, 10]);
+    var path = d3.geoPath()
+            .projection(projection);
+    var svg = d3.select("body")
+            .append("svg")
+            .attr("width", width)
+            .attr("height", height);
+    var g = svg.selectAll(".collection")
+            .data(habs.features)
+            .enter()
+            .append("g")
+            .attr("class", "collection");
+     svg.selectAll("path")
+          .data(habs.features).enter()
+          .append("path")
+          .attr("d", path)
+          .on("mouseover", function(d) {		
+            div.transition()		
+                .duration(200)		
+                .style("opacity", .75);		
+            div.html(d.properties.sciname + "    Status: " + 
+                     d.properties["status"])
+                .style("left", (d3.event.pageX) + "px")		
+                .style("top", (d3.event.pageY - 28) + "px")
+            console.log(d.properties.status)
+            console.log(d.properties.sciname);	
+            })
+          .on("mouseout", function(d) {		
+            div.transition()		
+                .duration(500)		
+                .style("opacity", 0);	
+        })
+          .on("click", function(d) {
+            window.open("https://en.wikipedia.org/wiki/" + d.properties.sciname, "_blank");
+          })
+          .style("stroke", "#98AF49")
+          .style("stroke-width", "3")
+          .style("fill", "#98AF49");
           console.log("feats: ", habs.features);
 };
 
